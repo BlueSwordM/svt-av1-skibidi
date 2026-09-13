16b9e367af8790b70671e4ca8497010746cb2e80
BlueSwordM <zakdjeb@gmail.com>
2026-09-13 15:20:01 -0400

    fix: adding the sharpness flag to light_rdoq and lowering rweight to fix the blocking bug

--- Source/Lib/Codec/full_loop.c
+++ Source/Lib/Codec/full_loop.c
@@ -1076,7 +1076,8 @@ static void svt_av1_optimize_b(PictureControlSet* pcs, ModeDecisionContext* ctx,
         }
     }
     if (light_rdoq) {
-        rweight = 10;
+        sharpness = 1;
+        rweight = 5;
     }
 
     const int64_t  rdmult = (((((int64_t)lambda * plane_rd_mult[is_inter][plane_type]) * rweight) / 100) + 2) >> rshift;
